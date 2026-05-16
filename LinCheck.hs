c5496a9c0675009eac308441272f3273fe64c6ce
Alina Banerjee <alina@glitchgirl.us>
2026-05-16 19:01:40 +0530

    Add error checking via throwError

--- LinCheck.hs
+++ LinCheck.hs
@@ -1,4 +1,8 @@
-module LinCheck where
+import Control.Monad.Except
+
+data TypeError = Err String deriving Show
+
+type Check = Either TypeError
 
 type Sym = String
 
@@ -10,10 +14,10 @@ data Term = Var String
           | LBool Bool
           deriving (Eq, Show, Ord)
 
-data Type = TBool
+data Type = TTrue
+           | TFalse
            | TInt
            | TArr Type Type
-           | TErr
           deriving (Show, Eq, Ord)
 
 type Context = [(Sym, Type)]
@@ -21,33 +25,34 @@ type Context = [(Sym, Type)]
 extend :: Context -> (String, Type) -> Context
 extend context vt = vt : context
 
-check :: Context -> Term -> Type
-check _ (LBool _) = TBool
-check _ (LInt _) = TInt
-check context (TIf t1 t2 t3) =
-    let result = check context t1 in
-    if result == TBool then check context t2
+check :: Context -> Term -> Check Type
+check _ (LBool True) = return TTrue
+check _ (LBool False) = return TFalse
+check _ (LInt _) = return TInt
+
+check context (TIf t1 t2 t3) = do
+    result <- check context t1
+    if result == TTrue then check context t2
     else check context t3
 
 check context (Var x) = case (lookup x context) of
-    Just a -> a
-    Nothing -> TErr
+    Just a -> return a
+    Nothing -> throwError $ Err "var not in context"
 
-check env (Lam x t e) =
-    let t' = (check (extend env (x, t)) e) in
-    TArr t t'
+check env (Lam x t e) = do
+    t' <- (check (extend env (x, t)) e)
+    return (TArr t t')
 
-check context (App e1 e2) =
-    let t1 = check context e1 in
-    let t2 = check context e2 in
+check context (App e1 e2) = do
+    t1 <- check context e1
+    t2 <- check context e2
     case t1 of
-      (TArr t1a t1b) | t1a == t2 -> t1b
-      (TArr t1a _) -> TErr
-      _ -> TErr
+      (TArr t1a t1b) | t1a == t2 -> return t1b
+      (TArr t1a _) -> throwError $ Err "Type mismatch"
+      _ -> throwError $ Err "Trying to apply non-function"
 
-
-
-
+checkExpr :: Term -> Either TypeError Type
+checkExpr x = check [] x
 
 
 
