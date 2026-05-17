a1c954963b8baf9aac787bea8b95b246ad56879d
Alina Banerjee <alina@glitchgirl.us>
2026-05-17 18:50:54 +0530

    Add type-checking for linear types

--- LinCheck.hs
+++ LinCheck.hs
@@ -6,53 +6,105 @@ type Check = Either TypeError
 
 type Sym = String
 
-data Term = Var String
-          | TIf Term Term Term
-          | Lam Sym Type Term
-          | App Term Term
-          | LInt Int
-          | LBool Bool
+data LinQual = Linear
+             | Unrestrict
           deriving (Eq, Show, Ord)
 
-data Type = TTrue
-           | TFalse
-           | TInt
-           | TArr Type Type
-          deriving (Show, Eq, Ord)
+data Term = Var String
+          | TIf Term Term Term
+          | Lam LinQual Sym QualType Term
+          | Pair LinQual Term Term
+          | App Term Term
+          | LInt LinQual Int
+          | LBool LinQual Bool
+          | Split Term Sym Sym Term
+          deriving (Eq, Show, Ord)
 
-type Context = [(Sym, Type)]
+data PreType = TBool
+             | TInt
+             | TArr QualType QualType
+             | TPair QualType QualType
+             deriving (Show, Eq, Ord)
 
-extend :: Context -> (String, Type) -> Context
+data QualType = QualType LinQual PreType
+          deriving (Eq, Show, Ord)
+
+type Context = [(Sym, QualType)]
+
+{-----------------------------------------------}
+
+extend :: Context -> (String, QualType) -> Context
 extend context vt = vt : context
 
-check :: Context -> Term -> Check Type
-check _ (LBool True) = return TTrue
-check _ (LBool False) = return TFalse
-check _ (LInt _) = return TInt
+diffContext :: Context -> String -> Context
+diffContext context v qt = do
+    case qt of
+    (QualType Linear _) ->
+        case (lookup v context) of
+            Just _ -> throwError $ Err ("Linear Var" ++  v ++ " unused")
+            Nothing -> return context
+    (QualType Unrestricted _) -> return (delete v context)
 
-check context (TIf t1 t2 t3) = do
-    result <- check context t1
-    if result == TTrue then check context t2
-    else check context t3
+{- All linear types are consumed when the term is type-checked in
+ - the input context and unrestricted types pass through unchanged -}
+check :: Context -> Term -> Check (QualType, Context)
 
-check context (Var x) = case (lookup x context) of
-    Just a -> return a
+check context (Var x) =
+  case (lookup x context) of
+    Just qt@(QualType Unrestricted  _)   -> return (qt, context)
+    Just qt@(QualType Linear _) as  -> return (qt, (delete x context)
     Nothing -> throwError $ Err "var not in context"
 
-check env (Lam x t e) = do
-    t' <- (check (extend env (x, t)) e)
-    return (TArr t t')
+check context (Lam q x qt body) = do
+    (bodyTy', context') <- check (extend context (x, qt)) body
+    context'' <- diffContext context' x qt
+    when (q == Unrestricted) $ unless (context == context'') $
+            throwError $ Err "Variables not captured in context"
+    return (QualType q (TArr qt bodyTy'), context'')
 
-check context (App e1 e2) = do
-    t1 <- check context e1
-    t2 <- check context e2
-    case t1 of
-      (TArr t1a t1b) | t1a == t2 -> return t1b
-      (TArr t1a _) -> throwError $ Err "Type mismatch"
-      _ -> throwError $ Err "Trying to apply non-function"
+check context (TIf t1 t2 t3) = do
+    (boolTy, context') <- check context t1
+    case boolTy of
+    Qualtype _ TBool ->
+         t1', context''  <- check context'' t2
+         t2', context''' <- check context'' t3
+         unless (t1' == t2')  $ throwError $ Err "Branch types differ"
+         unless (context'' == context''')
+                   $ throwError $ Err "Branch contexts differ"
+         return (ty2, context'')
+     _  -> throwError $ Err "Condition in If term does not typecheck to a boolean"
 
+check context (App t1 t2) = do
+    (resultTy, context') <- check context t1
+    case resultTy of
+     QualTpye _  (TArr qt1 qt2) -> do
+        (resultTy2, context'') <- check context' t2
+        unless (resultTy2 == qt1) $ throwError $ Err "Type mismatch in App"
+        return (qt2, context'')
+     _            -> throwError $ Err "Trying to apply non-function"
+
+check context (Pair qt t1 t2) = do
+    (pairTy1, context') <- check context t1
+    (pairTy2, context'')  <- check context' t2
+    return (QualType qt (TPair pairTy1 pairTy2), context'')
+
+check context (Split splitTerm x y InTerm) = do
+    (splitTy, context') <- check context splitTerm
+    case splitTy of
+    Qualtype _ TPair (qt1, qt2) ->
+        (_ , context'') <- pure (extend context' (x, qty1))
+        (_ , context''') <- pure (extend context' (y, qty2))
+        (typeRes, context'''') <- check context''' Interm
+        contextDiff1 <- diffContext context'''' x qt
+        contextDiff2 <- diffContext contextDiff1 x qt
+        return (typeRes, contextDiff2)
+    _ -> throwError $ Err "Cannot split a non-pair"
+
+
+check context (LBool q _) = return (QualType q TBool, context)
+check context (LInt q _) = return (QualType q TInt, context)
+
+{-----------------------------------------------}
 checkExpr :: Term -> Either TypeError Type
 checkExpr x = check [] x
-
-
 
